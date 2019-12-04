afeb7221b47402086a3b3bb2fe7430628b7a2cde
Zomdies <sablezubi@mail.ru>
2019-12-04 22:05:20 +0700

    commi_start

---
+++ precache-manifest.afb52e30a5a2a783e5f09311dcd5a05c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4754e03bb7bd711ac527d0b5ce1ab910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f95e9ccdad81af226e8c",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "37852402d3f4099fbb43",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f95e9ccdad81af226e8c",
+    "url": "/static/js/2.51009c2f.chunk.js"
+  },
+  {
+    "revision": "37852402d3f4099fbb43",
+    "url": "/static/js/main.47521418.chunk.js"
+  },
+  {
+    "revision": "32a15a4818fc2d8dd5ce",
+    "url": "/static/js/runtime-main.d91f2397.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
